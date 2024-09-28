bbf6678a76628cb8bb011adfdbbac58131dd6bf5
tioramadhn <tioramadhntio@apps.ipb.ac.id>
2024-09-28 14:12:57 +0700

    fix: linter

--- apps/enji.dev/src/hooks/useAnimateContent.ts
+++ apps/enji.dev/src/hooks/useAnimateContent.ts
@@ -1,21 +1,13 @@
 import { Content } from '@/contents/index/CleanIntuitive';
 import { useEffect, useState } from 'react';
-import useScreenSize from './useScreenSize';
 
 export const useAnimateContent = (content: Array<Content>) => {
   const [currentState, setCurrentState] = useState<Content>(content[0]);
   const [isUserClick, setIsUserClick] = useState(false);
-  const screenSize = useScreenSize();
 
   useEffect(() => {
     let animateState: NodeJS.Timer;
     let timeOutState: NodeJS.Timeout;
-    // if (screenSize < 1024) {
-    //   return () => {
-    //     clearInterval(animateState);
-    //     clearTimeout(timeOutState);
-    //   };
-    // }
 
     if (isUserClick) {
       timeOutState = setTimeout(() => {
